13128af36f00b36490092fab45bb17b33b39fb12
MicolGiannelli19 <micol.giannelli@outlook.com>
2023-11-15 09:07:17 +0000

    trying this again

--- docs/practice2.py
+++ docs/practice2.py
@@ -1,3 +1,6 @@
 class Micla:
     def method1():
         """Here is a doctring to check if this works I don't know if I should turn this into markdown"""
+
+    def methos19():
+        return "Hello"
